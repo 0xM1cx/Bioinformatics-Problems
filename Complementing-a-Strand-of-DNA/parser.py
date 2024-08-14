adacee1ccaee71954fddda320dd8c92b05a2b4da
0xm1cx <shawnlovecode14@gmail.com>
2024-08-14 13:40:05 +0800

    Solved new challenge

--- Complementing-a-Strand-of-DNA/parser.py
+++ Complementing-a-Strand-of-DNA/parser.py
@@ -1,4 +1,16 @@
-with open('') as f:
+with open('./rosalind_revc.txt') as f:
     content = f.read().strip()
-    for i in content:
-        if
+    content = content[::-1]
+    new_strand = ""
+    for n in content:
+        if n == 'A':
+            new_strand += 'T'
+        elif n == 'T':
+            new_strand += 'A'
+        elif n == 'G':
+            new_strand += 'C'
+        elif n == 'C':
+            new_strand += 'G'
+
+    print(new_strand)
+
